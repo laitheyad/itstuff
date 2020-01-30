e5e7b00a344c5bb8d9d2ddbaf0723fb53bcfd8e5
laitheyad <laithop@gmail.com>
2020-01-31 01:27:57 +0200

    serilizer fix

--- itstuff/serializers.py
+++ itstuff/serializers.py
@@ -7,17 +7,20 @@ class MajorSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['name',]
 
 class SubjectSerializer(serializers.HyperlinkedModelSerializer):
+    major= MajorSerializer(Major.objects.all())
     class Meta:
         model = Subject
         fields = ['name','level','major','description','type']
 
 class NotebookSerializer(serializers.HyperlinkedModelSerializer):
+    subject=SubjectSerializer(Subject.objects.all())
     class Meta:
         model = Notebook
         fields = ['name','author','description','link','subject']
 
 class TestbankSerializer(serializers.HyperlinkedModelSerializer):
-      class Meta:
+    subject = SubjectSerializer(Subject.objects.all())
+    class Meta:
         model = Testbank
         fields = ['name','type','reference','link','subject']
 

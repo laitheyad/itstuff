c8be26e5414d541c6b1eaafb0f7a4b98da28f2e2
laitheyad <laithop@gmail.com>
2020-01-31 00:52:42 +0200

    test

--- itstuff/models.py
+++ itstuff/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Major(models.Model):
-    name=models.CharField(max_length=150)
+    names=models.CharField(max_length=150)
     def __str__(self):
         return self.name
 
